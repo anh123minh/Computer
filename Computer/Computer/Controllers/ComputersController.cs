227921d3005bda099d32145bc06df0fafd0ad30d
anh123minh <phamanhminh231@gmail.com>
2020-05-22 13:42:01 +0300

    add ApiControllerBase

---
+++ Computer/Computer/Controllers/ComputersController.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Computer.Infrastructure.Core;
+using Computer.Service;
+using Computer.Model.Models;
+using Computer.Service;
+using Computer.Infrastructure.Core;
+using Computer.Infrastructure.Extensions;
+using Computer.Models;
+
+namespace Computer.Controllers
+{
+    [RoutePrefix("api/computer")]
+    [Authorize]
+    public class ComputerController : ApiControllerBase
+    {
+        private IComputerService _computerService;
+
+        public ComputerController(IErrorService errorService, IComputerService computerService) :
+            base(errorService)
+        {
+            this._computerService = computerService;
+        }
+
+        [Route("getall")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                var listCategory = _computerService.GetAll();
+
+                var listPostCategoryVm = Mapper.Map<List<ComputerViewModel>>(listCategory);
+
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listPostCategoryVm);
+
+                return response;
+            });
+        }
+
+        [Route("add")]
+        public HttpResponseMessage Post(HttpRequestMessage request, ComputerViewModel computerVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    Model.Models.Computer newComputer = new Model.Models.Computer();
+                    newComputer.UpdateComputer(computerVm);
+
+                    var computer = _computerService.Add(newComputer);
+                    _computerService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.Created, computer);
+                }
+                return response;
+            });
+        }
+
+        [Route("update")]
+        public HttpResponseMessage Put(HttpRequestMessage request, ComputerViewModel computerVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var computerDb = _computerService.GetById(computerVm.ID);
+                    computerDb.UpdateComputer(computerVm);
+                    _computerService.Update(computerDb);
+                    _computerService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+                return response;
+            });
+        }
+
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    _computerService.Delete(id);
+                    _computerService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK);
+                }
+                return response;
+            });
+        }
+    }
+}
---
+++ Computer/Computer/Infrastructure/Extensions/EntityExtentions.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Globalization;
+using Computer.Model.Models;
+using Computer.Models;
+
+namespace Computer.Infrastructure.Extensions
+{
+    public static class EntityExtensions
+    {
+        public static void UpdatePostCategory(this Model.Models.Computer computer, ComputerViewModel computerVm)
+        {
+            computer.ComputerId = computerVm.ID;
+            computer.ComputerCode = computerVm.Name;
+            computer.ComputerName = computerVm.Description;
+            computer.ComputerDescription = computerVm.Alias;
+            computer.ComputerTypeId = computerVm.ParentID;
+            computer.ProducerTypeId = computerVm.DisplayOrder;
+            computer.DeparmentTypeId = computerVm.Image;
+            computer.IsBusyNow = computerVm.HomeFlag;
+
+            computer.CreatedDate = computerVm.CreatedDate;
+            computer.CreatedBy = computerVm.CreatedBy;
+            computer.UpdatedDate = computerVm.UpdatedDate;
+            computer.UpdatedBy = computerVm.UpdatedBy;
+            computer.MetaKeyword = computerVm.MetaKeyword;
+            computer.MetaDescription = computerVm.MetaDescription;
+            computer.Status = computerVm.Status;
+        }
+        
+        public static void UpdateFunction(this Function function, FunctionViewModel functionVm)
+        {
+            function.Name = functionVm.Name;
+            function.DisplayOrder = functionVm.DisplayOrder;
+            function.IconCss = functionVm.IconCss;
+            function.Status = functionVm.Status;
+            function.ParentId = functionVm.ParentId;
+            function.Status = functionVm.Status;
+            function.URL = functionVm.URL;
+            function.ID = functionVm.ID;
+        }
+        public static void UpdatePermission(this Permission permission, PermissionViewModel permissionVm)
+        {
+            permission.RoleId = permissionVm.RoleId;
+            permission.FunctionId = permissionVm.FunctionId;
+            permission.CanCreate = permissionVm.CanCreate;
+            permission.CanDelete = permissionVm.CanDelete;
+            permission.CanRead = permissionVm.CanRead;
+            permission.CanUpdate = permissionVm.CanUpdate;
+        }
+
+        public static void UpdateApplicationRole(this AppRole appRole, ApplicationRoleViewModel appRoleViewModel, string action = "add")
+        {
+            if (action == "update")
+                appRole.Id = appRoleViewModel.Id;
+            else
+                appRole.Id = Guid.NewGuid().ToString();
+            appRole.Name = appRoleViewModel.Name;
+            appRole.Description = appRoleViewModel.Description;
+        }
+
+        public static void UpdateUser(this AppUser appUser, AppUserViewModel appUserViewModel, string action = "add")
+        {
+            appUser.Id = appUserViewModel.Id;
+            appUser.FullName = appUserViewModel.FullName;
+            if (!string.IsNullOrEmpty(appUserViewModel.BirthDay))
+            {
+                DateTime dateTime = DateTime.ParseExact(appUserViewModel.BirthDay, "dd/MM/yyyy", new CultureInfo("vi-VN"));
+                appUser.BirthDay = dateTime;
+            }
+
+            appUser.Email = appUserViewModel.Email;
+            appUser.Address = appUserViewModel.Address;
+            appUser.UserName = appUserViewModel.UserName;
+            appUser.PhoneNumber = appUserViewModel.PhoneNumber;
+            appUser.Gender = appUserViewModel.Gender == "True" ? true : false;
+            appUser.Status = appUserViewModel.Status;
+            appUser.Address = appUserViewModel.Address;
+            appUser.Avatar = appUserViewModel.Avatar;
+        }
+    }
+}
