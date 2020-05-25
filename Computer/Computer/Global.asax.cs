f850851e96977dd69b11a86604bd6f275680a4ac
anh123minh <phamanhminh231@gmail.com>
2020-05-25 23:10:41 +0300

    Update some file config

--- Computer/Computer/Global.asax.cs
+++ Computer/Computer/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -16,6 +17,16 @@ namespace Computer
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Context.Response.AppendHeader("Access-Control-Allow-Credentials", "true");
+            var referrer = Request.UrlReferrer;
+            if (Context.Request.Path.Contains("signalr/") && referrer != null)
+            {
+                Context.Response.AppendHeader("Access-Control-Allow-Origin", referrer.Scheme + "://" + referrer.Authority);
+            }
         }
     }
 }
