e0c7f85ae3f7bb107e3db78064499b6d2aadeab0
anh123minh <phamanhminh231@gmail.com>
2020-05-25 20:01:12 +0300

    Update Web.Models

---
+++ Computer/Computer/Models/DataContracts/SavePermissionRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Computer.Models.DataContracts
+{
+    public class SavePermissionRequest
+    {
+        public string FunctionId { set; get; }
+
+        public IList<PermissionViewModel> Permissions { get; set; }
+    }
+}
---
+++ Computer/Computer/Models/Post/PostTagViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Computer.Models
+{
+    public class PostTagViewModel
+    {
+        public int PostID { set; get; }
+
+        public string TagID { set; get; }
+
+        public virtual PostViewModel Post { set; get; }
+
+        public virtual TagViewModel Tag { set; get; }
+    }
+}
